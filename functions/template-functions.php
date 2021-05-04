1b400c3a755c3c2af0d2d11a85a518367c609a84
westlundhanna <westlund90@hotmail.com>
2021-05-04 19:09:12 +0200

    storefront & woocommerce hooks and templates

--- functions/template-functions.php
+++ functions/template-functions.php
@@ -1,8 +1,6 @@
 <?php
 defined('ABSPATH') or die;
 
-
-
 function add_fullwidth_hero_under_header() {
     if( have_rows('hero') ):
 		while ( have_rows('hero') ) : the_row();
@@ -11,4 +9,19 @@ function add_fullwidth_hero_under_header() {
 			endif;
 		endwhile;
 	endif;  
+}
+
+function storefront_remove_sidebar_shop_page() {
+
+	if ( is_shop() || is_tax( 'product_cat' ) || get_post_type() == 'product' ) {
+
+		 remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
+		 add_filter( 'body_class', 'storefront_remove_sidebar_class_body', 10 );
+	}
+}
+
+function storefront_remove_sidebar_class_body( $wp_classes ) {
+
+	$wp_classes[] = 'page-template-template-fullwidth-php';
+	return $wp_classes;
 }
