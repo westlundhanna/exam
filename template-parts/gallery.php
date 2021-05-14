9fd22bbda685e03eaabf55d1b189f786e9107cf3
westlundhanna <westlund90@hotmail.com>
2021-05-14 13:40:17 +0200

    changed style and layout of gallery

--- template-parts/gallery.php
+++ template-parts/gallery.php
@@ -1,21 +1,32 @@
 <?php 
+$term_id = get_queried_object()->term_id;
+$post_id = 'product_cat_'.$term_id;
 
-    if(have_rows('galleri')) {
-        while(have_rows('galleri')): the_row(); 
-            if(get_row_layout('bild')):
-                $image1 = get_sub_field('bild1');
-                $image2 = get_sub_field('bild2');
-                $button = get_sub_field('button');
-            endif;
-        endwhile;
-    }
-?>
-
-<section class="gallery-images">
-    <img class="gallery-image" src="<?php echo $image1; ?>" alt="">
-    <img class="gallery-image" src="<?php echo $image2; ?>" alt="">
-    <?php var_dump($button); ?>
-
-</section>        
+if(have_rows('galleri')): ?>
+    <section class="gallery-images"> <?php
+    while(have_rows('galleri')): the_row(); 
+        $image = get_sub_field('bild');
+        $title = get_sub_field('rubrik'); 
+        $buttons = get_sub_field('knapp'); 
+        foreach($buttons as $item) {
+            if($item['titel']) {
+                $btn_title = $item['titel'];
+            }
+            if($item['kategorilank']) {
+                foreach($item['kategorilank'] as $category) {
+                    $term_link = get_term_link($category, 'product_cat');
+                }
+            }
+        }
+        ?>  
+        <div class="gallery-image" style="background-image: url('<?php echo $image; ?>');">
+            <div class="gallery-content">
+                <h2 class="gallery-title"><?php echo $title; ?></h2>
+                <a href="<?php echo $term_link; ?>"><button><?php echo $btn_title; ?></button></a>
+            </div>
+        </div> <?php
+    endwhile; ?>
+    </section> <?php
+endif;      
 
 
