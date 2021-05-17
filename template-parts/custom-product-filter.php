84176954a48f5733a94b167472f770ffbf5033bb
westlundhanna <westlund90@hotmail.com>
2021-05-17 14:21:58 +0200

    Added top level product categories to filter

--- template-parts/custom-product-filter.php
+++ template-parts/custom-product-filter.php
@@ -2,7 +2,8 @@
     $sizes = get_terms('pa_storlek', ['filterby' => 'name']);
     $colors = get_terms('pa_farg', ['filterby' => 'name']);
     $styles = get_terms('pa_stil', ['filterby' => 'name']);
-    
+    $product_cats = get_terms( array('taxonomy' => 'product_cat', 'parent' => 0) );
+
     $taxonomy1   = 'pa_storlek';
     $label_name1 = wc_attribute_label( $taxonomy1 );
 
@@ -11,11 +12,35 @@
 
     $taxonomy3   = 'pa_stil'; 
     $label_name3 = wc_attribute_label( $taxonomy3 );
+
+    $taxonomy4 = 'Kategorier';
+    $label_name4 = wc_attribute_label( $taxonomy4 );
+
     
+     
 ?>
 <div class="wc_filtering_wrapper">
     <form id="filterproducts" method="get" action="#filterproducts">
         <div class="filter-select__container">
+            <?php 
+            if(is_shop()): 
+            ?>
+            <select name="filterby[product_cat]" id="category">
+                <option disabled="disabled" selected="selected"><?php echo $label_name4; ?></option>
+                <?php 
+                   if (!empty($product_cats) && !is_wp_error($product_cat)): ?>
+                    <?php
+                    foreach ($product_cats as $product_cat) :
+                        ?>
+                        <option value="<?= $product_cat->term_id; ?>">
+                            <?= $product_cat->name; ?>
+                        </option>
+                        <?php
+                    endforeach;
+                endif; 
+                ?>
+            </select>
+            <?php endif; ?>
             <select id ="size" name="filterby[pa_storlek]" class="filterby">
             <option disabled="disabled" selected="selected"><?php echo $label_name1; ?></option>
                 <?php
