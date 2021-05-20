02627b1cd85ede20c38622a39c6f9f18d4d4f3ed
westlundhanna <westlund90@hotmail.com>
2021-05-20 15:02:24 +0200

    added discount percentage to sale badge

--- functions/woocommerce-functions.php
+++ functions/woocommerce-functions.php
@@ -175,5 +175,23 @@ function woo_custom_order_form() {
           remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
      }
 }
-
-
+ 
+function add_discount_to_sale_badge() {
+   global $product;
+   if ( ! $product->is_on_sale() ) return;
+   if ( $product->is_type( 'simple' ) ) {
+      $max_percentage = ( ( $product->get_regular_price() - $product->get_sale_price() ) / $product->get_regular_price() ) * 100;
+   } elseif ( $product->is_type( 'variable' ) ) {
+      $max_percentage = 0;
+      foreach ( $product->get_children() as $child_id ) {
+         $variation = wc_get_product( $child_id );
+         $price = $variation->get_regular_price();
+         $sale = $variation->get_sale_price();
+         if ( $price != 0 && ! empty( $sale ) ) $percentage = ( $price - $sale ) / $price * 100;
+         if ( $percentage > $max_percentage ) {
+            $max_percentage = $percentage;
+         }
+      }
+   }
+   if ( $max_percentage > 0 ) echo "<span class='onsale'>-" . round($max_percentage) . "%</span>"; // If you would like to show -40% off then add text after % sign
+}
